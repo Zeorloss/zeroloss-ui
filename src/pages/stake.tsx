1fbb033d07b30bdffc47389a389a5c845f1dac5f
ebimo21 <ebi4jah15@gmail.com>
2023-06-08 12:26:55 +0100

    fixes

--- src/pages/stake.tsx
+++ src/pages/stake.tsx
@@ -16,13 +16,8 @@ import BigNumber from "bignumber.js";
 import { MaxUint256 } from "@ethersproject/constants";
 import { TailSpin } from 'react-loader-spinner';
 
-
-
-    // getuserPoolinfo takes user address
-
-
 const stake = () => {
-    const [zltBal, setZltBal] = useState(0);
+    const [zltBal, setZltBal] = useState<number>(0);
     const [loadingApproveNFT, setLoadingApproveNFT] = useState<boolean>(false);
     const [loadingApproveToken, setLoadingApproveToken] = useState<boolean>(false);
     const [loadingUnStakeToken, setLoadingUnStakeToken] = useState<boolean>(false);
@@ -34,11 +29,9 @@ const stake = () => {
     const [zltNFTStakedId, setZltNFTStakedId] = useState<number[]>([]);
     const [zltNFTToStakeId, setZltNFTToStakeId] = useState<number>(0);
     const [zltNFTToUnStakeId, setZltNFTToUnStakeId] = useState<number>(0);
-    const [decimals, setDecimal] = useState(9);
-    const [allowance, setAllowance] = useState(10);
+    const [decimals, setDecimal] = useState<number>(9);
+    const [allowance, setAllowance] = useState<number>(10);
     const [stakeAmount, setStakeAmount] = useState<string | number>(0);
-    const [NFTId, setNFTId] = useState<number>(0);
-    const [NFTUnstakeId, setNFTUnStakeId] = useState<number>(0);
     const [unStakeAmount, setUnStakeAmount] = useState<string | number>(0);
     const [stakeAmountInwei, setStakeAmountInwei] = useState<string | BigNumberEth>()
     const [unStakeAmountInwei, setUnStakeAmountInwei] = useState<string | BigNumberEth>()
@@ -200,7 +193,7 @@ const stake = () => {
         });
 
         
-    }, [account])
+    }, [account, refreshBalances])
 
     // read amount staked
     useEffect(()=>{
@@ -470,7 +463,9 @@ const stake = () => {
                             <p className='text-base font-bold'>APY</p>
                         </div>
                         <div className='basis-3/12 grow'>
-                            <button className=' bg-[#f08c00] m-auto block text-white py-2 px-1'>Get ZLT</button>
+                            <a href="https://pancakeswap.finance/add/0xF1288cF18B1FAaA35F40111c3E5d2f827e1E920E/0x05D8762946fA7620b263E1e77003927addf5f7E6">
+                                <button className=' bg-[#f08c00] m-auto block text-white py-2 px-1'>Get ZLT</button>
+                            </a>
                         </div>
                     </div>
 
@@ -504,13 +499,6 @@ const stake = () => {
                                 onClick={nftApproval ? handleStakeNFT: handleApproveNFT }
                                 className='bg-[#f08c00] p-3 rounded m-auto  my-3 flex items-center gap-2 '>{nftApproval? "Stake" : "Approve"} {loadingApproveNFT? <TailSpin width={30} height={30} color='white' /> : ""} {loadingStakeNFT ? <TailSpin width={30} height={30} color='white' /> : '' }</button>
                             )}
-
-                            {/* {active && isApproved && (
-                            <button
-                                disabled={isApproving}
-                                onClick={Number(allowance) >= Number(stakeAmount) ? handleStakeNFT : ()=>console.log("not working")}
-                                className='bg-[#f08c00] p-3 rounded m-auto block my-3'>{isApproved? "Stake" : "Approves"}</button>
-                            )} */}
                         </div>
                         {zltNFTStakedId.length>0 && (
                         <div className='basis-full sm:basis-5/12 max-w-[330px] bg-[#3e3d3d] p-2 m-auto my-4'>
@@ -532,12 +520,6 @@ const stake = () => {
                                 onClick={Number(allowance) >= Number(stakeAmount) ? ()=>console.log("done") : handleApprove}
                              className='bg-[#f08c00] p-3 rounded m-auto block my-3'>Approve</button>
                             )}
-                            {/* {!active && (
-                                <Fragment>
-                                <ConnectWalletButton className="hover:bg-white bg-[#f08c00] my-3" />
-                                <p className="text-sm text-center">Connect your wallet.</p>
-                                </Fragment>
-                            )} */}
 
                             {!active && (
                                 <Fragment>
@@ -562,71 +544,6 @@ const stake = () => {
                     </div>
                 </div>
             </div>
-            
-            {/* <div className='py-10 bg-[#323232] my-5 rounded-md shadow-lg w-11/12 m-auto'>
-                <div className='flex gap-2 items-center'>
-                    <img width={100} src='/cdn/Zeroloss logo.png' alt='' />
-                    <div>
-                        <p className="font-semibold text-xl">ZEROLOSS NFT</p>
-                        <p className='text-slate-400'>Staking</p>
-                    </div>
-                </div>
-                <div className='text-slate-200 font-semibold  text-xl gap-4 rounded-md flex items-center justify-center flex-wrap p-4 shadow-md '>
-                    <div className='text-center flex flex-wrap basis-full justify-center'>
-                        <div className='my-4 basis-5/12'>
-                            <span>10 LP</span>
-                            <p className='text-base font-bold'>Staked</p>
-                        </div>
-                        <div className='my-4 basis-5/12'>
-                            <span>1</span>
-                            <p className='text-base font-bold'># Staked</p>
-                        </div>
-                        <div className='my-4 basis-5/12'>
-                            <span>21%</span>
-                            <p className='text-base font-bold'>APY</p>
-                        </div>
-                        <div className='my-4 basis-5/12'>
-                            <span>$12,493</span>
-                            <p className='text-base font-bold'>LP Price</p>
-                        </div>
-                        
-                    </div>
-
-                    <div className='basis-full'>
-                        <button className=' bg-[#f08c00] m-auto block text-white py-3 px-4'>Get ZLT</button>
-
-                    </div>
-                </div>
-                <div className='flex flex-wrap justify-between items-center'>
-                    <div className='basis-full p-3 text-xl md:basis-[60%] flex flex-wrap justify-center items-center gap-2'>
-                        <div className='basis-full sm:basis-5/12 m-auto my-4'>
-                            <p className='text-2xl font-semibold'>Stake</p>
-                            <input placeholder='0' className='w-full bg-[#393939] p-2 my-4' type='number' />
-                            <div className='flex items-center justify-center gap-3'>
-                                <span className='py-2 px-4 border border-solid border-slate-500'>25%</span>
-                                <span className='py-2 px-4 border border-solid border-slate-500'>50%</span>
-                                <span className='py-2 px-4 border border-solid border-slate-500'>100%</span>
-                            </div>
-                            <button className='bg-[#f08c00] p-3 rounded m-auto block my-10'>Approve</button>
-                        </div>
-                        <div className='basis-full m-auto my-4'>
-                            <p className='text-2xl font-semibold'>Harvest</p>
-                            <input placeholder='0' className='w-full bg-[#393939] p-2 my-4' type='number' />
-                            <div className='flex items-center justify-center gap-3'>
-                                <span className='py-2 px-4 border border-solid border-slate-500'>25%</span>
-                                <span className='py-2 px-4 border border-solid border-slate-500'>50%</span>
-                                <span className='py-2 px-4 border border-solid border-slate-500'>100%</span>
-                            </div>
-                            <button className='bg-[#f08c00] p-3 rounded m-auto block my-10'>Approve</button>
-                        </div>
-                    </div>
-                    <div className='basis-full text-lg lg:basis-[30%] text-center'>
-                        <p>Pending Reward</p>
-                        <p className='font-bold my-2'>1.23 ZLT</p>
-                        <button className='px-4 py-3 tounded bg-white text-black font-bold'>Harvest</button>
-                    </div>
-                </div>
-            </div> */}
         </div>
     </Layout>
   )
